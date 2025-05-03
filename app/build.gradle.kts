dfd9242b35760fa502ac68a50d592bbc72abef31
zedsas <tusyuk01@mail.ru>
2025-05-03 22:05:42 +0700

    Подключил

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,14 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation ("androidx.core:core-ktx:1.10.1")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
